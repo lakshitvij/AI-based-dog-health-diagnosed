27bee31ca725f474da461c1ffeeff751f64603cf
lakshitvij <lakshitvij3@gmail.com>
2026-03-22 09:42:07 +0530

    Update default port for running the Flask app
    Change default port from 8000 to 10000 for app run.

--- app.py
+++ app.py
@@ -501,6 +501,6 @@ def predict_image_api():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    port = int(os.environ.get('PORT', 8000))
-    app.run(debug=os.environ.get('FLASK_ENV') != 'production',
-            host='0.0.0.0', port=port)
+
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
